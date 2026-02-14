ce02979f648b66eb70793a2dd9139d8553bc9e76
makkuz2013 <maksimkuziakov@gmail.com>
2026-02-14 16:39:54 +0200

    added items

--- src/main/java/lt/makkuz2013/testmod/TestMod.java
+++ src/main/java/lt/makkuz2013/testmod/TestMod.java
@@ -1,5 +1,6 @@
 package lt.makkuz2013.testmod;
 
+import lt.makkuz2013.testmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class TestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
